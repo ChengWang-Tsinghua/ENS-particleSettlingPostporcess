dc914ade7ecf6ecffb7e30e1b24e7e20b8965ea1
chengwang <70124651+ChengWang-Tsinghua@users.noreply.github.com>
2024-01-09 11:45:34 +0800

    Add files via upload

---
+++ neighborFun/neighborIdx3.m
@@ -0,0 +1,59 @@
+function [neighborIdxAll] = neighborIdx2(particle_part,tracer_part,idx1,idx2,Rmin,Rmax)
+
+particle.xf = particle_part.Xf(idx1);
+particle.yf = particle_part.Yf(idx1);
+particle.zf = particle_part.Zf(idx1);
+particle.vx = particle_part.Vx(idx1);
+particle.vy = particle_part.Vy(idx1);
+particle.vz = particle_part.Vz(idx1);
+
+tracer.xf = tracer_part.Xf(idx2);
+tracer.yf = tracer_part.Yf(idx2);
+tracer.zf = tracer_part.Zf(idx2);
+
+%%
+Xp = [particle.xf, particle.yf, particle.zf];
+Vp = [particle.vx, particle.vy, particle.vz];
+
+R = rotationMatrix(Vp, [1,0,0]);
+
+rXp = (R*Xp')';
+
+for i = 1:numel(tracer.xf)
+    Xf = [tracer.xf(i), tracer.yf(i), tracer.zf(i)];
+    rXf = (R*Xf')';
+    
+    [rho, theta, z] = cartToCylind(rXp, rXf);
+end
+
+
+%% neighboring tracers
+
+neighborIdxAll = struct('idx',[],'d',[]);
+neighborIdxAll.Rmin = Rmin;
+neighborIdxAll.Rmax = Rmax;
+
+
+for i = 1:numel(tracer.xf)
+    d(i,:) = sqrt((particle.xf-tracer.xf(i))^2+(particle.yf-tracer.yf(i))^2+(particle.zf-tracer.zf(i))^2);
+end
+
+%% global neighboring
+idx01 = find(d>Rmin & d<Rmax);
+neighborIdxAll.idx = idx2(idx01);
+neighborIdxAll.d = d(idx01);
+
+
+%% front and back 
+rpt.x = tracer.xf(idx01) - particle.xf;
+rpt.y = tracer.yf(idx01) - particle.yf;
+rpt.z = tracer.zf(idx01) - particle.zf;
+
+rv = particle.vx*rpt.x + particle.vy*rpt.y + particle.vz*rpt.z;
+neighborIdxAll.idxfront = neighborIdxAll.idx(rv>0);
+neighborIdxAll.idxback = neighborIdxAll.idx(rv<0);
+
+
+
+
+
