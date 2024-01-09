8eb679a516f47f2009943b173b93dcbb429da92b
chengwang <70124651+ChengWang-Tsinghua@users.noreply.github.com>
2024-01-09 11:43:38 +0800

    Update findTerminalState.m

--- neighborFun/findTerminalState.m
+++ neighborFun/findTerminalState.m
@@ -1,29 +1,34 @@
-function [maxTimeLength,startTime,endTime,Thres] = findTerminalState(part,RelError)
+function [maxTimeLength, startTime, endTime, Thres] = findTerminalState(particle_part, RelError)
+% FINDTERMINALSTATE identifies the terminal state of particles based on relative error.
+%   [maxTimeLength, startTime, endTime, Thres] = FINDTERMINALSTATE(particle_part, RelError)
+%   analyzes particle data to find the terminal state based on specified relative error.
+%
+%   Input:
+%       particle_part - Structure array containing particle information.
+%       RelError      - Relative error threshold for identifying the terminal state.
+%
+%   Output:
+%       maxTimeLength - Maximum time length of the identified terminal state.
+%       startTime     - Start time of the terminal state.
+%       endTime       - End time of the terminal state.
+%       Thres         - Relative error threshold used for identification.
+%
 
-fields = fieldnames(part);
-for i = 1:length(fields)
-    part2.(fields{i}) = vertcat(part.(fields{i}));
+    % Maximum number of frames
+    Nframemax = 1e6;
+
+    % Calculate experiment numbers for each particle
+    for i = 1:numel(particle_part)
+        nexp(i,:) = fix(particle_part(i).Tf/Nframemax) + 1;
+    end
+
+%     % Unique experiment numbers
+%     uni_exp = unique(nexp);
+
+    % Call the findTimeRange function to get time range and threshold
+    [maxTimeLength, startTime, endTime, Thres] = findTimeRange(particle_part.Ay, RelError);
+
+%     % Adjust start and end times based on experiment numbers
+%     startTime = startTime + Nframemax * (uni_exp - 1);
+%     endTime = endTime + Nframemax * (uni_exp - 1);
 end
-
-Nframemax = 1e6;
-% NTrackmax = 1e9;
-for i = 1:numel(part2)
-    nexp(i,:) = fix(part2(i).Tf/Nframemax)+1;
-%     t(i,:) = mod(part2(i).Tf,Nframemax)/fps;
-end
-uni_exp = unique(nexp);
-Nexp = numel(uni_exp);
-
-
-maxTimeLength = zeros(Nexp,1);
-startTime = zeros(Nexp,1);
-endTime  = zeros(Nexp,1);
-Thres  = zeros(Nexp,1);
-
-for j = 1:Nexp
-    idx = find(nexp==j);
-    [maxTimeLength(j), startTime(j), endTime(j),Thres(j)] = findTimeRange(part2.Ay(idx), RelError);
-    startTime(j) = startTime(j) +  Nframemax *(j-1);
-    endTime(j)   = endTime(j)   +  Nframemax *(j-1);
-end
-
