2fe96dd3d0ee7366d6d34940944f2e77d30b4af3
chengwang <70124651+ChengWang-Tsinghua@users.noreply.github.com>
2024-01-09 11:41:53 +0800

    Add files via upload

--- SphericalMesh.m
+++ SphericalMesh.m
@@ -1,11 +1,30 @@
-function [r,theta,rgrid,tgrid,X,Y] = SphericalMesh(radius,nbinr,nbint, theta)
-    % Default values if not provided
-    
+function [r, theta, rgrid, tgrid, X, Y] = SphericalMesh(radius, nbinr, nbint, theta)
+% SPHERICALMESH generates a spherical mesh grid in Cartesian coordinates.
+%   [r, theta, rgrid, tgrid, X, Y] = SPHERICALMESH(radius, nbinr, nbint, theta)
+%   creates a spherical mesh grid with specified radial and azimuthal coordinates.
+%
+%   Input:
+%       radius - Vector [Rmin, Rmax] specifying the radial bounds.
+%       nbinr  - Number of bins in the radial direction.
+%       nbint  - Number of bins in the azimuthal direction.
+%       theta  - Optional parameter specifying the azimuthal angle range.
+%
+%   Output:
+%       r      - Radial coordinates.
+%       theta  - Azimuthal coordinates.
+%       rgrid  - Mesh grid of radial coordinates.
+%       tgrid  - Mesh grid of azimuthal coordinates.
+%       X      - Cartesian X coordinates.
+%       Y      - Cartesian Y coordinates.
+%
+
+    % Default values for nbinr and nbint if not provided
     if nargin < 2
         nbinr = 20;
         nbint = 20;
     end
     
+    % Default azimuthal angle range if not provided
     if nargin < 4
         thetaMin = 0;
         thetaMax = pi;
@@ -14,8 +33,10 @@ function [r,theta,rgrid,tgrid,X,Y] = SphericalMesh(radius,nbinr,nbint, theta)
         thetaMax = theta(2);
     end
     
+    % Extract radial bounds
     Rmin = radius(1);
     Rmax = radius(2);
+
     % Define spherical grid parameters
     r = linspace(Rmin, Rmax, nbinr); % radial coordinates
     theta = linspace(thetaMin, thetaMax, nbint); % azimuthal coordinates
@@ -27,26 +48,21 @@ function [r,theta,rgrid,tgrid,X,Y] = SphericalMesh(radius,nbinr,nbint, theta)
     X = rgrid .* cos(tgrid);
     Y = rgrid .* sin(tgrid);
 
-%     % Plot the 2D mesh with radial and azimuthal grid lines
+    % Uncomment the following lines to visualize the 2D mesh with grid lines
 %     figure;
 %     plot(X, Y, '.');
 %     hold on;
-% 
-%     % Connect points along the azimuthal angle to show azimuthal grid lines
 %     for i = 1:size(X, 2)
 %         plot(X(:, i), Y(:, i), '-k');
 %     end
-% 
-%     % Connect points along the radial direction to show radial grid lines
 %     for i = 1:size(X, 1)
 %         plot(X(i, :), Y(i, :), '-k');
 %     end
-% 
 %     hold off;
-% 
 %     title('Spherical Mesh Grid (2D) with Grid Lines');
 %     xlabel('X');
 %     ylabel('Y');
 %     axis equal;
 %     grid on;
 end
+
--- slipVeloCylinderical.m
+++ slipVeloCylinderical.m
@@ -1,31 +1,67 @@
-function slipVeloCylind = slipVeloCylinderical(particle_part,tracers_part,neighborIdxAll,startTime,endTime,k)
+function slipVeloCylind = slipVeloCylinderical(particle_part, tracers_part, neighborIdxAll, startTime, endTime, k)
+% SLIPVELOCYLINDERICAL computes slip velocities in cylindrical coordinates
+% for tracers near particles between specified start and end times.
+%   slipVeloCylind = SLIPVELOCYLINDERICAL(particle_part, tracers_part,
+%   neighborIdxAll, startTime, endTime, k) calculates slip velocities based
+%   on input parameters and stores the results in the structure 'slipVeloCylind'.
+%
+%   Input:
+%       particle_part   - Structure containing information about particles.
+%       tracers_part    - Structure containing information about tracers.
+%       neighborIdxAll  - Structure containing neighbor information.
+%       startTime       - Start time for slip velocity calculation.
+%       endTime         - End time for slip velocity calculation.
+%       k               - Frame-related parameter for calculations.
+%
+%   Output:
+%       slipVeloCylind  - Structure containing slip velocity information.
+%           .rho        - Radial distance in cylindrical coordinates.
+%           .theta      - Azimuthal angle in cylindrical coordinates.
+%           .z          - Axial distance in cylindrical coordinates.
+%           .Urel       - Relative slip velocity in cylindrical coordinates.
+%
 
-%% terminal state range
-Nframemax = 1e6;
+    Nframemax = 1e6;
 
-kexp = 1;
-if nargin>5
-    kexp = k;
-end
-tstart = mod(startTime(kexp),Nframemax);
-tend = mod(endTime(kexp),Nframemax);
+    % Set default value for frame-related parameter
+    kexp = 1;
+    if nargin > 5
+        kexp = k;
+    end
 
-slipVeloCylind = struct('rho',[],'theta',[],'z',[],'Urel',[],'Urelnorm',[]);
-%% prepare idx for searching
-idx_front_back = sort(vertcat(neighborIdxAll(tstart:tend).idx));
+    % Calculate start and end times within the frame limit
+    tstart = mod(startTime(kexp), Nframemax);
+    tend = mod(endTime(kexp), Nframemax);
 
-for i = 1:numel(idx_front_back)
-    idxp = particle_part.Tf == tracers_part.Tf(idx_front_back(i));
-    idxt = idx_front_back(i);
-    
-    Xp = [particle_part.Xf(idxp), particle_part.Yf(idxp), particle_part.Zf(idxp)];
-    Vp = [particle_part.Vx(idxp), particle_part.Vy(idxp), particle_part.Vz(idxp)];
-    Xf = [tracers_part.Xf(idxt), tracers_part.Yf(idxt), tracers_part.Zf(idxt)];
-    Uf = [tracers_part.Vx(idxt), tracers_part.Vy(idxt), tracers_part.Vz(idxt)];
-    normVp = Vp/norm(Vp);
+    % Initialize structure to store slip velocity information
+    slipVeloCylind = struct('rho',[], 'theta',[], 'z',[], 'Urel',[]);
 
-    RM = rotationMatrix(Vp, [1,0,0]);
-    
-    [slipVeloCylind(i,:).rho, slipVeloCylind(i,:).theta, slipVeloCylind(i,:).z] = cartToCylind(Xp, Xf, RM);
-    slipVeloCylind(i,:).Urel = Uf.*normVp-Vp;
+    % Prepare indices for searching
+    idx_front_back = sort(vertcat(neighborIdxAll(tstart:tend).idx));
+
+    % Iterate over indices
+    for i = 1:numel(idx_front_back)
+        idxp = particle_part.Tf == tracers_part.Tf(idx_front_back(i));
+        idxt = idx_front_back(i);
+
+        % Extract position and velocity information for particles and tracers
+        Xp = [particle_part.Xf(idxp), particle_part.Yf(idxp), particle_part.Zf(idxp)];
+        Vp = [particle_part.Vx(idxp), particle_part.Vy(idxp), particle_part.Vz(idxp)];
+        Xf = [tracers_part.Xf(idxt), tracers_part.Yf(idxt), tracers_part.Zf(idxt)];
+        Uf = [tracers_part.Vx(idxt), tracers_part.Vy(idxt), tracers_part.Vz(idxt)];
+
+        % Rotate coordinates to align the particle velocity with the z-axis
+        R = rotationMatrix(Vp, [0,0,1]);
+        rXp = (R*Xp')';
+        rXf = (R*Xf')';
+        rVp = (R*Vp')';
+        rUf = (R*Uf')';
+        norm_rVp = rVp/norm(rVp);
+
+        % Convert Cartesian coordinates to cylindrical coordinates
+        [slipVeloCylind(i,:).rho, slipVeloCylind(i,:).theta, slipVeloCylind(i,:).z] = cartToCylind(rXp, rXf);
+
+        % Calculate relative slip velocity in cylindrical coordinates
+        slipVeloCylind(i,:).Urel = rUf.*norm_rVp - rVp;
+    end
 end
