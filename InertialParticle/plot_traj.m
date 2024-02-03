f36b39a058ff0a9ee74c9a9c206ce53fe91e7d50
chengwang <70124651+ChengWang-Tsinghua@users.noreply.github.com>
2024-02-03 15:33:16 +0800

    Add files via upload

--- InertialParticle/plot_traj.m
+++ InertialParticle/plot_traj.m
@@ -1,85 +1,77 @@
-function plot_traj(part_particle,titlestring,axisrange,ifsave,fout)
-
-%%
-fields = fieldnames(part_particle);
-for i = 1:length(fields)
-    part2.(fields{i}) = vertcat(part_particle.(fields{i}));
-end
-
-if ~exist(fout)
-    mkdir(fout)
-end
+function plot_traj(tracklong_particle,origin,axisrange,ifsave,fout)
 
 scale = 0.008;
-%%
-% axisrange = [-5 25 -18 10 -40 40];
+Nexp = max(size(tracklong_particle));
 
-Nframemax = 1e6;
-nexp= fix(part2.T/Nframemax)+1;
-uni_exp = unique(nexp);
-Nexp = numel(uni_exp);
+
+x0 = origin(1);
+z0 = origin(3);
 
 mycolormap = mycolor('#B10000','#FFFFFF','#0000B2');
 mycolormap2 = mycolor('#063970','#eeeee4','#e28743');
 mycolorind = 1:floor(size(mycolormap,1)/Nexp):size(mycolormap,1);
 %% all trajectories 3D plot
-figure;
-scatter3(part2.Xf,part2.Zf,part2.Yf,abs(part2.Ay)*scale,part2.Vy,'filled',LineWidth=2)
+figure
+for kexp = 1:Nexp
+    scatter3(tracklong_particle(kexp).Zf-z0,tracklong_particle(kexp).Xf-x0,tracklong_particle(kexp).Yf,abs(tracklong_particle(kexp).Ay)*scale,tracklong_particle(kexp).Vy,'filled',LineWidth=2);hold on
+end
 axis equal;box;grid on;axis padded
 set(gca,FontSize=15)
 legend('$size \propto Acce. $','interpreter','latex',FontWeight='bold',FontSize=15)
-title(titlestring,'interpreter','latex',FontWeight='bold',FontSize=18)
-zlabel('$y(mm)$','interpreter','latex',FontWeight='bold',FontSize=18)
-ylabel('$z(mm)$','interpreter','latex',FontWeight='bold',FontSize=18)
-xlabel('$x(mm)$','interpreter','latex',FontWeight='bold',FontSize=18)
+% title(titlestring,'interpreter','latex',FontWeight='bold',FontSize=18)
+zlabel('$y [mm]$','interpreter','latex',FontWeight='bold',FontSize=18)
+ylabel('$x [mm]$','interpreter','latex',FontWeight='bold',FontSize=18)
+xlabel('$z [mm]$','interpreter','latex',FontWeight='bold',FontSize=18)
 legend(Location='northeast')
-colormap(mycolormap);col =colorbar;
+colormap(mycolormap);
+col =colorbar;
+col.TickLabelInterpreter = "latex";
 ylabel(col,'$Vy(m/s)$','interpreter','latex',FontWeight='bold',FontSize=18)
 axis(axisrange)
 
 if ifsave == 1
-    savefig([fout '\traj3D'])
-    saveas(gcf,[fout,'\traj3D'],'png')
-    saveas(gcf,[fout,'\traj3D'],'pdf')
+    savefig_custom(fout,'traj3D',8,7)
 end
 %% all trajectories 2D plot - xz projection 
-figure;
-scatter(part2.Xf,part2.Zf,abs(part2.Ay)*scale,part2.Vy,'filled',LineWidth=2);hold on
-scatter(part2.Xf(1),part2.Zf(1),abs(part2.Ay(1))*scale*0.2,'go',MarkerFaceColor='g',LineWidth=2)
+figure
+for kexp = 1:Nexp
+    scatter(tracklong_particle(kexp).Xf-x0,tracklong_particle(kexp).Zf-z0,abs(tracklong_particle(kexp).Ay)*scale,tracklong_particle(kexp).Vy,'filled',LineWidth=2);hold on
+end
+scatter(0,0,'go',MarkerFaceColor='g',LineWidth=2)
 axis equal;box;grid;axis padded
 set(gca,FontSize=15)
 legend('$size \propto Acce. $','interpreter','latex',FontWeight='bold',FontSize=15)
-title(titlestring,'interpreter','latex',FontWeight='bold',FontSize=18)
-ylabel('$z(mm)$','interpreter','latex',FontWeight='bold',FontSize=18)
-xlabel('$x(mm)$','interpreter','latex',FontWeight='bold',FontSize=18)
-colormap(mycolormap);col =colorbar;
+% title(titlestring,'interpreter','latex',FontWeight='bold',FontSize=18)
+ylabel('$z [mm]$','interpreter','latex',FontWeight='bold',FontSize=18)
+xlabel('$x [mm]$','interpreter','latex',FontWeight='bold',FontSize=18)
+colormap(mycolormap);
+col =colorbar;
+col.TickLabelInterpreter = "latex";
 ylabel(col,'$Vy(m/s)$','interpreter','latex',FontWeight='bold',FontSize=18)
 axis(axisrange(1:4))
 
-if ifsave ==1
-    savefig([fout '\traj2Dxz'])
-    saveas(gcf,[fout,'\traj2Dxz'],'png')
-    saveas(gcf,[fout,'\traj2Dxz'],'pdf')
+if ifsave == 1
+    savefig_custom(fout,'traj2Dxz',8,7)
 end
+
 %% xz projection
-figure;
-for i = 1:Nexp
-    plot(part2.Xf(nexp==i),part2.Zf(nexp==i),Color=mycolormap2(mycolorind(uni_exp(i)),:),LineWidth=2);hold on
+figure
+for kexp = 1:Nexp
+    plot(tracklong_particle(kexp).Xf-x0,tracklong_particle(kexp).Zf-z0,Color=mycolormap2(mycolorind(kexp),:),LineWidth=2);hold on
 end
-plot(part2.Xf(1),part2.Zf(1),'go',MarkerFaceColor='g',LineWidth=2);hold on
+scatter(0,0,'go',MarkerFaceColor='g',LineWidth=2)
 axis equal;box on;grid on;axis padded
 set(gca,FontSize=15)
-title(titlestring,'interpreter','latex',FontWeight='bold',FontSize=18)
-ylabel('$z(mm)$','interpreter','latex',FontWeight='bold',FontSize=18)
-xlabel('$x(mm)$','interpreter','latex',FontWeight='bold',FontSize=18)
+% title(titlestring,'interpreter','latex',FontWeight='bold',FontSize=18)
+ylabel('$z [mm]$','interpreter','latex',FontWeight='bold',FontSize=18)
+xlabel('$x [mm]$','interpreter','latex',FontWeight='bold',FontSize=18)
 colormap(mycolormap2)
 col = colorbar('Ticks',[0 0.25 0.5 0.75 1],'TickLabels',ceil([0.01 0.25 0.5 0.75 1]*Nexp),FontSize=12,Location='north');
+col.TickLabelInterpreter = "latex";
 ylabel(col,'Num. Exp.')
 set(col,'position',[0.65 0.2 0.2 0.02])
 axis(axisrange(1:4))
 
 if ifsave == 1
-    savefig([fout '\traj2Dxz2'])
-    saveas(gcf,[fout,'\traj2Dxz2'],'png')
-    saveas(gcf,[fout,'\traj2Dxz2'],'pdf')
+    savefig_custom(fout,'traj2Dxz2',8,7)
 end
